84743c98f581e5c4a0dbaa24144b86c29e064e80
miccou <30512204+miccou@users.noreply.github.com>
2024-03-13 16:57:00 +1100

    wip

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div class="h-screen w-screen flex flex-row bg-gray-100">
+  <div class="h-full w-32 bg-green-100 p-4">sidenav</div>
+  <div class="h-screen w-full bg-blue-100 flex flex-col">
+    <div class="bg-blue-100 h-16 w-full p-4">top bar</div>
+    <div class=" w-full bg-white overflow-y-auto h-fit p-8">
+      <div class="w-full h-48 mb-8 bg-red-100 rounded p-4">top content</div>
+      <div class="w-full flex flex-row gap-8">
+        <div class="w-3/5 flex flex-col gap-8">
+          <div class="bg-red-100 h-56 rounded p-4">content</div>
+          <div class="bg-red-100 h-80 rounded p-4">content</div>
+          <div class="bg-red-100 h-80 rounded p-4">content</div>
+          <div class="bg-red-100 h-80 rounded p-4">content</div>
+        </div>
+        <div class="w-2/5 h-[2000px] bg-yellow-100 rounded p-4">
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+          <p>content</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
